15adf2cb05b35b6d5ec2800ec565e58dfe451925
Jan Sandbrink <nobodysnightmare@gmail.com>
2016-12-10 19:41:29 +0100

    Make index of readings more usable
    - Show latest entries first
    - Paginate to ensure smooth performance

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'haml-rails'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'therubyracer', platforms: :ruby
+gem 'kaminari'
 
 gem 'omniauth', '~> 1.3'
 gem 'omniauth-google-oauth2'
--- app/controllers/inverter_readings_controller.rb
+++ app/controllers/inverter_readings_controller.rb
@@ -3,7 +3,10 @@ class InverterReadingsController < ApplicationController
   helper_method :inverter
 
   def index
-    @readings = inverter.inverter_readings.order(:time)
+    @readings = inverter.inverter_readings
+                        .order(time: :desc)
+                        .page(params[:page])
+                        .per(100)
   end
 
   private
