1e38764169ba86c56346b36846672adcfa76492c
Jan Sandbrink <nobodysnightmare@gmail.com>
2016-12-10 11:36:48 +0100

    Display energy generated today

--- app/presenters/inverter_overview_presenter.rb
+++ app/presenters/inverter_overview_presenter.rb
@@ -2,7 +2,7 @@
 class InverterOverviewPresenter < SimpleDelegator
   def current_power
     @current_power ||= begin
-      now, before = inverter_readings.order(time: :desc).limit(2).to_a
+      before, now = latest_cycle
       if now && before
         time_passed = now.time - before.time
         generated = now.value - before.value
@@ -14,7 +14,27 @@ class InverterOverviewPresenter < SimpleDelegator
     end
   end
 
+  def daily_generation
+    @daily_generation ||= begin
+      day_start = inverter_readings.where('time > ?', Date.today.to_time)
+                                   .order(:time)
+                                   .first
+      if day_start
+        latest_reading.value - day_start.value
+      else
+        0
+      end
+    end
+  end
+
   def latest_reading
-    @latest_reading ||= inverter_readings.order(:time).last
+    latest_cycle.last
+  end
+
+  def latest_cycle
+    @latest_cycle ||= inverter_readings.order(time: :desc)
+                                       .limit(2)
+                                       .to_a
+                                       .reverse
   end
 end
