800933788aac7a138baeae69e59e79b49b638663
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-03-21 18:55:42 +0100

    Fix existing specs

--- spec/factories/site.rb
+++ spec/factories/site.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 FactoryGirl.define do
-  factory :meter do
+  factory :site do
     name { "#{Faker::Book.genre} #{Faker::Pokemon.name}" }
-    serial { Faker::Code.imei }
-
-    site
   end
 end
