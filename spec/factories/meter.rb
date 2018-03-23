2daf7bba7d61e488c9641c33ce550c02b0cd7386
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-03-23 07:48:36 +0100

    Make name collisions in specs unlikely
    I actually got a few of those by now...
    Sadly there are not enough Pokemon names
    it seems...

--- spec/factories/meter.rb
+++ spec/factories/meter.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :meter do
-    name { "#{Faker::Book.genre} #{Faker::Pokemon.name}" }
+    name { "#{Faker::Book.genre} #{Faker::Pokemon.name} #{rand(10_000)}" }
     serial { Faker::Code.imei }
 
     site
--- spec/factories/site.rb
+++ spec/factories/site.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :site do
-    name { "#{Faker::Book.genre} #{Faker::Pokemon.name}" }
+    name { "#{Faker::Book.genre} #{Faker::Pokemon.name} #{rand(10_000)}" }
   end
 end
