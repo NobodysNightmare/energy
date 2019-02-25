24b15106909101d411fc3f30f3b5cb9c582b0db3
Jan Sandbrink <nobodysnightmare@gmail.com>
2019-02-25 08:07:00 +0100

    Indicate pending request by disabling button
    That's at least a small indicator, that an async
    request is in progress.
    
    Now also hiding the result view for subsequent requests that
    errored.

--- app/assets/javascripts/find_readings_around.js
+++ app/assets/javascripts/find_readings_around.js
@@ -11,30 +11,41 @@ class FindReadingsAround {
     }
 
     initialize() {
-        $(this.submitButton).on("click", () => this.updateValues());
+        this.submitButton.on("click", () => this.updateValues());
     }
 
     updateValues() {
-        $.ajax({ url: "/meters/" + this.meterId + "/readings/around", data: { time: this.dateInput.val() } })
-            .done((data) => {
-                this.resultContainer.removeClass("hidden");
+        this.uiIndicateLoading();
+        let request = $.ajax({ url: "/meters/" + this.meterId + "/readings/around", data: { time: this.dateInput.val() } });
+        request.always(() => this.uiIndicateDone());
+        request.done((data) => {
+            this.resultContainer.removeClass("hidden");
 
-                if(data.before) {
-                    this.beforeContainer.find(".finder-result-time").text(data.before.time);
-                    this.beforeContainer.find(".finder-result-value").text(data.before.value);
-                } else {
-                    this.beforeContainer.find(".finder-result-time").text("-");
-                    this.beforeContainer.find(".finder-result-value").text("-");
-                }
+            if(data.before) {
+                this.beforeContainer.find(".finder-result-time").text(data.before.time);
+                this.beforeContainer.find(".finder-result-value").text(data.before.value);
+            } else {
+                this.beforeContainer.find(".finder-result-time").text("-");
+                this.beforeContainer.find(".finder-result-value").text("-");
+            }
 
-                if(data.after) {
-                    this.afterContainer.find(".finder-result-time").text(data.after.time);
-                    this.afterContainer.find(".finder-result-value").text(data.after.value);
-                } else {
-                    this.afterContainer.find(".finder-result-time").text("-");
-                    this.afterContainer.find(".finder-result-value").text("-");
-                }
-            });
+            if(data.after) {
+                this.afterContainer.find(".finder-result-time").text(data.after.time);
+                this.afterContainer.find(".finder-result-value").text(data.after.value);
+            } else {
+                this.afterContainer.find(".finder-result-time").text("-");
+                this.afterContainer.find(".finder-result-value").text("-");
+            }
+        });
+        request.fail(() => this.resultContainer.addClass("hidden"));
+    }
+
+    uiIndicateLoading() {
+        this.submitButton[0].disabled = true;
+    }
+
+    uiIndicateDone() {
+        this.submitButton[0].disabled = false;
     }
 }
 
